7324e237d69c0e66d2c7ecafad1d43535c174608
Bennani Othmane <bennmane2712@outlook.fr>
2024-02-20 09:48:56 +0100

    Add test_utils

--- test_utils.py
+++ test_utils.py
@@ -3,12 +3,26 @@ import utils
 
 def test_fact():
     # À compléter...
+
+    # Teste si la fonction lève une exception pour une valeur négative
+    with pytest.raises(ValueError):
+        utils.fact(-1)
+
     pass
 
 def test_roots():
     # À compléter...
+
+    # Teste si la fonction retourne une liste de deux éléments
+    assert len(utils.roots(1, 0, -1)) == 2
+
+
     pass
 
 def test_integrate():
     # À compléter...
+
+    # Teste si la fonction retourne une valeur proche de 8/3
+    assert abs(utils.integrate('x ** 2 - 1', -1, 1) - 8/3) < 1e-6
+    
     pass
